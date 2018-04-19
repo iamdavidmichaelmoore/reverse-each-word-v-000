7489725b5eefc13f312549562a0a3219d51376e8
David Moore <hey@iamdavidmoore.com>
2018-04-19 14:01:31 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@ def reverse_each_word(string)
   reversed_str = ""
   str_to_array = string.split(" ")
   str_to_array.collect do |word|
-    word.reverse
     temp_array = []
     temp_array << word.reverse
     reversed_str = temp_array.join(" ")
